78d5f5adb2bec6cc86aa84ecbdf7e5e0ab2dadc2
Callum Macpherson <callummac@protonmail.com>
2019-05-18 11:33:17 +0100

    Merge fixes

--- src/board/pieceColors.js
+++
@@ -1,9 +0,0 @@
-const colors =  {
-    'yellow': 'hsl(60, 80%, 50%)',
-    'red': 'hsl(0, 50%, 50%)',
-    'green': 'hsl(100, 50%, 50%)',
-    'blue': 'hsl(220, 70%, 50%)',
-    'grey': 'hsl(220, 5%, 70%)',
-};
-
-export default colors;
