84b8313ca4a522a84f07adea0706dfb75ddd0cb5
Callum Macpherson <callummac@protonmail.com>
2019-06-01 17:12:10 +0100

    socket get user

--- server/socket/protectSocket.js
+++ server/socket/protectSocket.js
@@ -1,5 +1,7 @@
 const protectSocket = (socket, next) => {
     if (!socket.handshake.session.user) return next(new Error('No session'));
+
+    socket.request.user = socket.handshake.session.user;
     next();
 };
 
