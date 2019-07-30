ab83ad64297cde65133e4a658cdc9ca712aaf349
Callum Macpherson <callummac@protonmail.com>
2019-07-30 21:36:41 +0100

    Fixed logging

--- node/logger.js
+++ node/logger.js
@@ -22,7 +22,7 @@ module.exports = createLogger({
         // - Write to all logs with level `info` and below to `combined.log`
         // - Write all logs error (and below) to `error.log`.
         //
-        new transports.File({ filename: 'error.log', level: 'error' }),
-        new transports.File({ filename: 'combined.log' }),
+        new transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new transports.File({ filename: 'logs/combined.log' }),
     ],
 });
